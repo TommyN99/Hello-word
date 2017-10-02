1a5b03f9de97376c8ce66804dacdb2ce06fcc8ec
TommyN99 <32265999+TommyN99@users.noreply.github.com>
2017-10-02 14:31:49 +0100

    Add files via upload

---
+++ practical 3.py
@@ -0,0 +1,8 @@
+print("Hello, this is a calculating system")
+number1 = int(input("Please enter a number"))
+print("Thank you")
+number2 = int(input("Please enter another number"))
+print("Thanks again!")
+print(number1 + number2)
+
+
